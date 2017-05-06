76a501a8d49cb35da6debaa1a9fa23ca4f6c19a4
Anastasia Gaydashenko <a.v.gaydashenko@gmail.com>
2017-05-06 16:55:49 +0300

    class State

---
+++ State.py
@@ -0,0 +1,47 @@
+class State:
+    def __init__(self, x, y, k):
+        self.x = x
+        self.y = y
+        self.k = k
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y
+
+    def __gt__(self, other):
+        if self.k.first() - 0.00001 > other.k.first():
+            return True
+        elif self.k.first() < other.k.first() - 0.00001:
+            return False
+        return self.k.second() > other.k.second()
+
+    def __le__(self, other):
+        if self.k.first() < other.k.first():
+            return True
+        elif self.k.first() > other.k.first():
+            return False
+        return self.k.second() < other.k.second() + 0.00001
+
+    def __lt__(self, other):
+        if self.k.first() + 0.000001 < other.k.first():
+            return True
+        elif self.k.first() - 0.000001 > other.k.first():
+            return False
+        return self.k.second() < other.k.second()
+
+    def __cmp__(self, other):
+        if self.k.first() - 0.00001 > other.k.first():
+            return 1
+        elif self.k.first() < other.k.first() - 0.00001:
+            return -1
+        if self.k.second() > other.k.second():
+            return 1
+        elif self.k.second() < other.k.second():
+            return -1
+        return 0
+
+    def __hash__(self):
+        return self.x + 34245 * self.y
+
